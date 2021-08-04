518a9a265eb517e30dc63d308a380ee5eaf36f0a
asagaorino0 <xcnsn724@yahoo.co.jp>
2021-08-04 19:19:14 +0900

    [add]level2 途中

---
+++ proaca-function/functions/src/index.ts
@@ -0,0 +1,125 @@
+import * as functions from "firebase-functions";
+
+// // Start writing Firebase Functions
+// // https://firebase.google.com/docs/functions/typescript
+//
+// export const helloWorld = functions.https.onRequest((request, response) => {
+//   functions.logger.info("Hello logs!", {structuredData: true});
+//   response.send("Hello from Firebase!");
+// });
+// The Firebase Admin SDK to access Firestore.
+const admin = require(`firebase-admin`);
+admin.initializeApp();
+
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+// Automatically allow cross-origin requests
+app.use(cors({ origin: true }));
+export const proacaHello = functions.https.onRequest((req,
+    res) => {
+    app.use(cors({ origin: true }));
+    console.log("hello!!!")
+    res.json({ message: "Hello world" })
+})
+
+export const getUserInfo = functions.https.onRequest((req, res) => {
+    res.json(
+        [
+            {
+                "id": 1,
+                "name": "Leanne Graham",
+                "username": "Bret",
+                "email": "Sincere@april.biz",
+                "address": {
+                    "street": "Kulas Light",
+                    "suite": "Apt. 556",
+                    "city": "Gwenborough",
+                    "zipcode": "92998-3874",
+                    "geo": {
+                        "lat": "-37.3159",
+                        "Ing": "81.1496"
+                    }
+                },
+                "phone": "1-770-736-8031 x56442",
+                "website": "hildegard.org",
+                "company": {
+                    "name": "Romaguera-Crona",
+                    "catchPhrase": "Multi-layered client-server neural-net",
+                    "bs": "harness real-time e-markets"
+                }
+            },
+            {
+                "id": 2,
+                "name": "Ervin Howell",
+                "username": "Antonette",
+                "email": "Shanna@melissa.tv",
+                "address": {
+                    "street": "Victor Plains",
+                    "suite": "Suite 879",
+                    "city": "Wisokyburgh",
+                    "zipcode": "90566-7771",
+                    "geo": {
+                        "lat": "-43.9509",
+                        "lng": "-34.4618"
+                    }
+                },
+                "phone": "010-692-6593 x09125",
+                "website": "anastasia.net",
+                "company": {
+                    "name": "Deckow-Crist",
+                    "catchPhrase": "Proactive didactic contingency",
+                    "bs": "synergize scalable supply-chains"
+                }
+            },
+            {
+                "id": 3,
+                "name": "Clementine Bauch",
+                "username": "Samantha",
+                "email": "Nathan@yesenia.net",
+                "address": {
+                    "street": "Douglas Extension",
+                    "suite": "Suite 847",
+                    "city": "McKenziehaven",
+                    "zipcode": "59590-4157",
+                    "geo": {
+                        "lat": "-68.6102",
+                        "lng": "-47.0653"
+                    }
+                },
+                "phone": "1-463-123-4447",
+                "website": "ramiro.info",
+                "company": {
+                    "name": "Romaguera-Jacobson",
+                    "catchPhrase": "Face to face bifurcated interface",
+                    "bs": "e-enable strategic applications"
+                }
+            },
+            {
+                "id": 4,
+                "name": "Patricia Lebsack",
+                "username": "Karianne",
+                "email": "Julianne.OConner@kory.org",
+                "address": {
+                    "street": "Hoeger Mall",
+                    "suite": "Apt. 692",
+                    "city": "South Elvis",
+                    "zipcode": "53919-4257",
+                    "geo": {
+                        "lat": "29.4572",
+                        "lng": "-164.2990"
+                    }
+                },
+                "phone": "493-170-9623 x156",
+                "website": "kale.biz",
+                "company": {
+                    "name": "Robel-Corkery",
+                    "catchPhrase": "Multi-tiered zero tolerance productivity",
+                    "bs": "transition cutting-edge web services"
+                }
+            }
+        ]
+    )
+})
--- proaca-web/src/screens/HomeScreen.tsx
+++ proaca-web/src/screens/HomeScreen.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import * as functions from "../../../proaca-function/functions/node_modules/firebase-functions";
 import { Button, Text, View } from "react-native";
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -6,13 +7,59 @@ import SubScreen from '../screens/SubScreen'
 import ThirdScreen from '../screens/ThirdScreen';
 
 
+
 function HomeScreen({ navigation }: { navigation: any }) {
+
+    const onClick = () => {
+
+        var request = new XMLHttpRequest();
+        request.open('GET', 'https://us-central1-react-native-firebase-15517.cloudfunctions.net/proacaHello?text=uppercasemetoo', true);
+        // request.open('GET', 'https://jsonplaceholder.typicode.com/users/1', true);
+        request.responseType = 'json';
+
+        request.onload = function () {
+            var data = this.response;
+            console.log(data);
+        };
+        request.send();
+
+
+        // fetch('https://us-central1-react-native-firebase-15517.cloudfunctions.net/getUserInfo?text=uppercasemetoo', {
+        //     // fetch("https://jsonplaceholder.typicode.com/users", {
+        //     method: "GET",
+        // }).then(response => response.text())
+        //     .then(text => {
+        //         console.log(text);
+        //     });
+
+
+        // let requestURL = "https://us-central1-react-native-firebase-15517.cloudfunctions.net/proacaHello";
+        // // let request = new XMLHttpRequest();
+        // // request.open('GET', requestURL);
+        // // request.responseType = 'text'; // now we're getting a string!
+        // // request.send();
+        // // // request.onload = function () {
+        // // const superHeroesText = request.response; // get the string from the response
+        // // const superHeroes = JSON.parse(superHeroesText || "null"); // convert it to an object
+        // // console.log(superHeroes);
+        // // }
+        // // const object = { requestURL };
+        // const object = { requestURL };
+        // const myjson = JSON.stringify(object || "null");
+        // const newjson = JSON.parse(myjson || "null");
+        // console.table(newjson);
+        // console.log(myjson);
+    }
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
             <Text>Home Screen</Text>
             <Button
                 title="Go to Sub"
                 onPress={() => navigation.navigate('Sub')}
+            />
+            <Button
+                title="Get user info"
+                onPress={onClick}
             />
         </View>
     )
